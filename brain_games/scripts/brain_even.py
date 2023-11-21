39ff664344bba373f24484e564fa346eabb2cc5d
Evgeniy Kolpakov <eskolpakov@gmail.com>
2023-11-22 00:33:20 +0300

    Fixed errors in the brain-even function

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -11,7 +11,7 @@ def main():
 
 
 def even_or_not():
-    name = prompt.string('May I have you name? ')
+    name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
     correct_answer = 0
@@ -22,11 +22,12 @@ def even_or_not():
         if (write_answer == "yes" and number % 2 == 0) or (write_answer == "no" and number % 2 != 0):
             print("Correct!")
             correct_answer += 1
-	if correct_answer == 3:
-	    print(f"Congratulations, {name}!")
         else:
-            print(f"'{write_answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}!")
-	    return
+            print(f"'{write_answer}' is wrong answer ;(. Correct answer was 'yes'." 
+                  if number % 2 == 0 else f"'{write_answer}' is wrong answer ;(. Correct answer was 'no'.")
+            print(f"Let's try again, {name}!")
+            return
+    print(f"Congratulations, {name}!")
 
 
 if __name__ == '__main__':
